dc8a096ac5e8a34bfe908b220414f216553e33ec
hanakirin <alfred.craftworks@gmail.com>
2024-06-30 09:59:44 +0900

    Changed to read API key from configuration file

--- ipinfo/source/ipinfoRead.py
+++ ipinfo/source/ipinfoRead.py
@@ -1,5 +1,6 @@
 import sys
 import requests
+import configparser
 
 def get_ip_info(ip_address, token):
     url = f"https://ipinfo.io/{ip_address}/json"
@@ -14,10 +15,16 @@ def get_ip_info(ip_address, token):
         return None
 
 if __name__ == "__main__":
-    ip_address = sys.argv[1]
-    token = "xxx"  # ここにIPinfoのAPIキーを入力してください
-    ip_info = get_ip_info(ip_address, token)
-    
+    config_ini = configparser.ConfigParser()
+    config_ini.read('config.ini', encoding='utf-8')
+    api_key = config_ini['IPINFO']['api_key']
+
+    if len(sys.argv) == 2:
+        ip_address = sys.argv[1]
+    else:
+        print("Argument Error: python ipinfoRead.py {IP Address}") 
+
+    ip_info = get_ip_info(ip_address, api_key)
     if ip_info:
         print("IP Information:")
         for key, value in ip_info.items():
